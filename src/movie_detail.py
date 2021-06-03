a96780ed6978362cc777c986bc5c42de0b68f489
tuhinsaud4614 <tuhinsaud4614@gmail.com>
2021-06-04 03:26:55 +0600

    movie detail scrawler complete

--- src/movie_detail.py
+++ src/movie_detail.py
@@ -4,6 +4,12 @@ import requests as rq
 from bs4 import BeautifulSoup as BS
 
 
+class Movie:
+    def __init__(self, url):
+        self.__req = rq.get(url)
+        self.__soup = BS(self.__req.content, "html.parser")
+
+
 req = rq.get(
     'https://m.imdb.com/title/tt8332922/', 'html.parser')
 soup = BS(req.content, 'html.parser')
@@ -48,6 +54,75 @@ def get_casts(sel):
     return temp
 
 
+def get_release_date(sel):
+    text = sel.get_text().strip().lower()
+    if text:
+        # date format May 28 2011
+        result_text = re.findall(
+            r"([a-z]{3} [0-9]{2}\, [0-9]{4})", text)
+        if len(result_text):
+            date = result_text[0].replace(",", "").split(" ")
+            if len(date) == 3:
+                return {"day": date[1], "month": date[0], "year": date[2]}
+    return None
+
+
+def get_detail(sel):
+    data = []
+    if len(sel):
+        for o in sel:
+            text = o.get_text()
+            if text:
+                data.append(text.strip().lower())
+
+    return data
+
+
+def get_filming_locations(sel):
+    text = sel.get_text()
+    if text and "," in text:
+        return [l.lower() for l in text.split(", ")]
+    return None
+
+
+def get_production_companies(sel):
+    if sel:
+        li_tags = sel.find_all("li")
+        if len(li_tags):
+            a_tags = []
+            for t in li_tags:
+                a = t.find("a")
+                if a:
+                    text = a.get_text()
+                    link_text = a.get("href")
+                    id = re.findall(r".*\/company\/(.*)\?.*", link_text)
+                    if text and len(id):
+                        a_tags.append({"id": id[0], "name": text.strip().lower()})
+            return a_tags if len(a_tags) else None
+    return None
+
+def get_world_gross(sel):
+    if sel:
+        li_tag = sel.find("li")
+        if li_tag:
+            span_tag = li_tag.find("span")
+            if span_tag:
+                text = span_tag.get_text()
+                if text:
+                    return text.strip().replace(",", "")
+    return None
+
+def get_tech_spec(sel):
+    a_tags = sel.find_all("a")
+    if len(a_tags):
+        data = []
+        for a in a_tags:
+            text = a.get_text()
+            if text:
+                data.append(text.strip().lower())
+        return data if len(data) else None
+    return None
+
 movie = dict()
 
 title_selector = soup.find(
@@ -67,7 +142,24 @@ writers = credits[1].find_all("a")
 
 cast_selectors = soup.find_all(
     'div', attrs={'data-testid': 'title-cast-item'})
-# print(img_selector)
+release_date_selectors = soup.find(
+    'li', attrs={'data-testid': 'title-details-releasedate'}).find_all("a")[1]
+country_of_origin_selectors = soup.find(
+    'li', attrs={'data-testid': 'title-details-origin'}).find_all("a")
+languages_selectors = soup.find(
+    'li', attrs={'data-testid': 'title-details-languages'}).find_all("a")
+filming_locations_selectors = soup.find(
+    'li', attrs={'data-testid': 'title-details-filminglocations'}).find_all("a")[1]
+production_companines_selectors = soup.find(
+    'li', attrs={'data-testid': 'title-details-companies'})
+gross_world_wide_selectors = soup.find(
+    'li', attrs={'data-testid': 'title-boxoffice-cumulativeworldwidegross'})
+color_selectors = soup.find(
+    'li', attrs={'data-testid': 'title-techspec_color'})
+sound_mix_selectors = soup.find(
+    'li', attrs={'data-testid': 'title-techspec_soundmix'})
+
+# print(get_tech_spec(sound_mix_selectors))
 
 movie["title"] = title_selector.get_text().strip().lower()
 movie["img"] = img_selector.get("src") if img_selector else None
@@ -78,6 +170,13 @@ movie["genres"] = [i.get_text().strip() for i in genres]
 movie["directors"] = get_credits(directors)
 movie["writers"] = get_credits(writers)
 movie["cast"] = get_casts(cast_selectors)
+movie["release_date"] = get_release_date(release_date_selectors)
+movie["country_of_origins"] = get_detail(country_of_origin_selectors)
+movie["languages"] = get_detail(languages_selectors)
+movie["filming_locations"] = get_filming_locations(filming_locations_selectors)
+movie["production_companies"] = get_production_companies(production_companines_selectors)
+movie["gross_world_wide"] = get_world_gross(gross_world_wide_selectors)
+movie["tech_spec"] = {"color": get_tech_spec(color_selectors), "sound_mix": get_tech_spec(sound_mix_selectors)}
 # print(movie)
 with open("movie.json", "w") as f:
     json.dump(movie, f, indent=2)
